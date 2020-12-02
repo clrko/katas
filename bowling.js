92e348959948dd092d8fe50aea5f932b5db05337
Thibaut Gery <thibaut.gery@datadoghq.com>
2020-12-02 22:07:37 +0100

    Impl spare & correct logic

--- bowling.js
+++ bowling.js
@@ -1,17 +1,33 @@
 function bowlingScore(rolls) {
   let score = 0;
-  let bonusRollsRemaining = 0;
-  for (const pinFell of rolls) {
+  let isSecondRollInFrame = false;
+  let frameNumber = 1;
+
+  for (let i = 0; i < rolls.length && frameNumber <= 10; i++) {
+    const pinFell = rolls[i];
+    const prevPinFell = rolls[i - 1] || 0;
+
     score += pinFell;
-    if (bonusRollsRemaining > 0) {
-      score += pinFell;
-      bonusRollsRemaining--;
-    }
-    if (pinFell === 10) {
-      bonusRollsRemaining = 2;
+    if (isStrike(pinFell)) {
+      score += (rolls[i + 1] || 0) + (rolls[i + 2] || 0);
+      isSecondRollInFrame = false;
+      frameNumber++;
+    } else if (isSpare(prevPinFell, pinFell, isSecondRollInFrame)) {
+      score += rolls[i + 1] || 0;
+      isSecondRollInFrame = false;
+      frameNumber++;
+    } else {
+      if (isSecondRollInFrame) {
+        frameNumber++;
+      }
+      isSecondRollInFrame = !isSecondRollInFrame;
     }
   }
   return score;
 }
+
+const isStrike = (pinFell) => pinFell === 10;
+const isSpare = (prevPinFell, pinFell, isSecondRoll) =>
+  isSecondRoll && prevPinFell + pinFell === 10;
 
 module.exports = bowlingScore;
